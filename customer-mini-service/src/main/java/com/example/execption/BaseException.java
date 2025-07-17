ce387656c54bcd4fc94586c94e9cf93a9abb5e2d
Bekir Tuğra Karabulut <129181669+BekirTugraKarabulut@users.noreply.github.com>
2025-07-17 17:50:23 +0300

    Delete customer-mini-service/src/main/java/com/example/execption directory

--- customer-mini-service/src/main/java/com/example/execption/BaseException.java
+++
@@ -1,8 +0,0 @@
-package com.example.execption;
-
-public class BaseException extends RuntimeException {
-
-    public BaseException(ErrorMessage errorMessage) {
-        super(errorMessage.prepareErrorMessage());
-    }
-}
